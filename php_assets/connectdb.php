07b461489a4daf9240fc4a0a742775f74cf1ef8b
Julien Biechler <julien.bibich@gmail.com>
2022-12-08 14:18:08 +0100

    Update connectdb.php

--- php_assets/connectdb.php
+++ php_assets/connectdb.php
@@ -4,8 +4,8 @@
   $password = "";
   $dbname = "videotheque";
 
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
+//  $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new PDO('mysql:host=$servername;dbname=$dbname', $userame, $password);
   if ($conn->connect_error) {
     die("La connexion a échouée: " . $conn->connect_error);
   }
