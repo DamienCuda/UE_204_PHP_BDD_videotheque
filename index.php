a2ecfa56729e4b64765d54acbc0e71f9e6d2b55d
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2022-12-09 05:48:44 +0100

    update

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
 
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-dismiss="modal">Compris</button>
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">SE CONNECTER</button>
                     </div>
                 </div>
             </div>
