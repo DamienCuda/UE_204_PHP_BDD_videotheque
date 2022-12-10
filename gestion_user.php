4502095d7c5be4b507bd885b4d642f1fd8515701
Nagamo <julien.bibich@gmail.com>
2022-12-10 16:05:42 +0100

    Gestion users add

--- gestion_user.php
+++ gestion_user.php
@@ -1,15 +1,44 @@
-<?php session_start(); ?>
+<?php session_start(); 
+    require_once("php_assets/connectdb.php"); ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <?php include 'php_assets/head.php'?>
-
+<?php include 'php_assets/header.php'?>
 <body>
-    <?php include 'php_assets/header.php'?>
-    <main>
-        
-    </main>
-    <?php include 'php_assets/footer.php'?>
+<main>
+<?php
+    $userReq = $conn->prepare('SELECT * FROM utilisateurs');
+    $userReq->execute();
+    $users = $userReq->fetchAll();
+?>
+<div class="container">
+<table class="table table-hover">
+    <thead>
+        <tr>
+          <th>ID</th>
+          <th>login</th>
+          <th>email</th>
+          <th>Admin (1= Oui, 0= Non)</th>
+          <th>Suppression</th>
+          <th>Modifier</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($users as $user){ ?>
+        <tr>
+          <td><?= $user['id']; ?></td>
+          <td><?= $user['login']; ?></td>
+           <td><?= $user['email']; ?></td>
+           <td><?= $user['is_admin']; ?></td>
+           <td><a href="/php_assets/delete_user.php?id=<?= $user['id']; ?>"<><button>Supprimer</button></a></td>
+           <td><a href="modif_user.php?id=<?= $user['id']; ?>"<><button>Modifier</button></a></td>
+        </tr>
+        <?php } ?>
+    </tbody>
+</table>  
+        </div>   
+</main>
+<?php include 'php_assets/footer.php'?>
 </body>
 </html>
---
+++ php_assets/delete_user.php
@@ -0,0 +1,8 @@
+<?php session_start();
+    require_once("connectdb.php");
+    require ("fonctions.php");
+
+$pdoDe = $conn->prepare('SELECT * FROM utilisateurs WHERE id= ?');
+        $pdoDe->execute(array(nettoyage($_GET['id'])));
+        header("Location: ../gestion_user.php");
+?>
