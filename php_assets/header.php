ef9cd32efd6c8d9e3743e6fbb12e93ef3221ecdc
Damien Cuda <contact@damiencuda.com>
2022-12-10 14:42:20 +0100

    modif header

--- php_assets/header.php
+++ php_assets/header.php
@@ -6,7 +6,7 @@
                 <div class="row align-items-center">
         ';
 
-        if(str_contains($_SERVER['REQUEST_URI'], 'index.php')){
+        if(str_contains($_SERVER['PHP_SELF'], 'index.php')){
             $header .= '
                         <div class="site-logo col-12 text-center">
                             <img class="navbar-brand img-fluid img_index" src="assets/Logo.png" alt="Vidéothèque Groupe 9">
