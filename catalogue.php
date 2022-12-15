39f213312f69ac3a8629615e590a6edded1a9f4c
Julien Biechler <julien.bibich@gmail.com>
2022-12-15 12:10:45 +0100

    add catalogue gestion

--- catalogue.php
+++ catalogue.php
@@ -284,9 +284,88 @@ $movies = $movieDisplay->fetchAll(PDO::FETCH_ASSOC);
         } else {
             ?>
             <div class="row">
-                <!-- Remplace ça par le tableau de gestion admin -->
-                <div class="col-12 mt-5" style="color:#fff;">Vous êtes admin</div>
-                <!-- Remplace ça par le tableau de gestion admin -->
+            <?php
+                $filmReq = $conn->prepare('SELECT * FROM catalogue');
+                $filmReq->execute();
+                $films = $filmReq->fetchAll();
+            ?>
+                <section id="gestion_film" class="mt-5">
+                    <table class="table table-striped responsive nowrap" id="gestion_filù_table">
+                        <thead>
+                        <tr>
+                            <th class="text-center"></th>
+                            <th class="text-center">ID</th>
+                            <th class="text-center">Poster</th>
+                            <th class="text-center">Titre</th>
+                            <th class="text-center">Réalisateur</th>
+                            <th class="text-center">Acteurs</th>
+                            <th class="text-center">Genre</th>
+                            <th class="text-center">Date de sortie</th>
+                            <th class="text-center">Durée</th>
+                            <th class="text-center">Synopsis</th>
+                            <th class="text-center">Prix</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($films as $film){
+                            ?>
+                            <tr class="<?php if($film['id'] == $_SESSION['id']){ echo "you"; } ?>">
+                                <td id="<?= $film['id']; ?>" class="checkbox_table"></td>
+                                <td><?= $film['id']; ?></td>
+                                <td><?= $film['movie_picture']; ?></td>
+                                <td><?= $film['title']; ?></td>
+                                <td><?= $film['director']; ?></td>
+                                <td><?= $film['acteurs']; ?></td>
+                                <td><?= $film['genre']; ?></td>
+                                <td><?= $film['release_year']; ?></td>
+                                <td><?= $film['duration']; ?></td>
+                                <td><?= $film['synopsis']; ?></td>
+                                <td><?= $film['price']; ?></td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                        <tfoot>
+                            <tr id="add_film_row">
+
+                                <?php
+                                    $pdoID = $conn->prepare("SELECT MAX(id) AS max_id FROM catalogue");
+                                    $pdoID->execute();
+                                    $pdoID = $pdoID->fetch();
+                                    $max_incrementation = $pdoID['max_id'] + 1;
+                                ?>
+
+                                <td></td>
+                                <td><?= $max_incrementation ?></td>
+                                <td><div class="profil-membre" style="background: url('img/profil_img/avatar.jpg');"></div></td>
+                                <td style="height: 60px">
+                                    <input type="text" name="title_add" id="title_add" class="form-control" placeholder="Nom du film...">
+                                </td>
+                                <td>
+                                    <input type="text" name="director_add" id="director_add" class="form-control" placeholder="Nom du réalisateur...">
+                                </td>
+                                <td>
+                                    <input type="text" name="acteurs_add" id="acteurs_add" class="form-control" placeholder="Nom des acteurs...">
+                                </td>
+                                <td>
+                                    <input type="text" name="genre_add" id="genre_add" class="form-control" placeholder="Genres du film...">
+                                </td>
+                                <td>
+                                    <input type="text" name="year_add" id="year_add" class="form-control" placeholder="Année de sortie...">
+                                </td>
+                                <td>
+                                    <input type="text" name="duration_add" id="duration_add" class="form-control" placeholder="Durée du film...">
+                                </td>
+                                <td>
+                                    <input type="textarea" name="synopsis_add" id="synopsis_add" class="form-control" placeholder="Synopsis du film...">
+                                </td>
+                                <td>
+                                    <input type="text" name="price_add" id="price_add" class="form-control" placeholder="Prix du film...">
+                                </td>
+                                <td><button class="btn btn-warning" style="background: #ffca2c !important;" id="add_film_btn">Ajouter</button></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </section>
             </div>
             <?php
         }
