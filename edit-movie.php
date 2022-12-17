d5c9098f15a9cd880d0997f59dc8360963ff8c2a
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2022-12-17 06:14:00 +0100

    permissions

--- edit-movie.php
+++ edit-movie.php
@@ -4,6 +4,10 @@ require("php_assets/verif_session_connect.php");
 require("php_assets/fonctions.php");
 require("php_assets/permission.php");
 
+if($is_admin === false){
+    header("location: index.php");
+}
+
 // On récupère les infos du film.
 $movieData = $conn->prepare('SELECT * FROM catalogue WHERE id = ?');
 $movieData->execute([
